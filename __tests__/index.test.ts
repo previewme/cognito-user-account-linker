28fd2bfe4a61cb197b41efb3ed42e6a9304639b6
Bhavik Kumar <bhavikk85@gmail.com>
2021-09-19 15:35:28 +1200

    Initial commit for Cognito account linking lambda

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,203 @@
-describe('this test should be replaced', () => {
-    test('is true', () => {
-        expect(true).toBeTruthy();
+import { handler } from '../src';
+import { PreSignUpTriggerEvent } from 'aws-lambda';
+import { PreSignUpExternalProviderTriggerEvent } from 'aws-lambda/trigger/cognito-user-pool-trigger/pre-signup';
+
+let mockListUsers = jest.fn();
+
+let mockCreateUser = jest.fn();
+
+const mockLinkUser = jest.fn(() => {
+    return {
+        promise: jest.fn(() => Promise.resolve())
+    };
+});
+
+const mockSetUserPassword = jest.fn(() => {
+    return {
+        promise: jest.fn(() => Promise.resolve())
+    };
+});
+
+jest.mock('aws-sdk', () => {
+    return {
+        CognitoIdentityServiceProvider: jest.fn(() => {
+            return {
+                listUsers: mockListUsers,
+                adminLinkProviderForUser: mockLinkUser,
+                adminCreateUser: mockCreateUser,
+                adminSetUserPassword: mockSetUserPassword
+            };
+        })
+    };
+});
+
+describe('AWS Cognito account linking', () => {
+    const cognitoNativeEvent: PreSignUpTriggerEvent = {
+        version: '1',
+        region: 'eu-central-1',
+        userPoolId: 'eu-central-1_7HPNLvT',
+        userName: 'c8302f6d-4469-b31e-36ee6239e267',
+        callerContext: {
+            awsSdkVersion: 'aws-sdk-unknown-unknown',
+            clientId: '741igvddli8mdl2v0bpsqc'
+        },
+        triggerSource: 'PreSignUp_SignUp',
+        request: {
+            userAttributes: {
+                given_name: 'Test',
+                family_name: 'User',
+                email: 'test@gmail.com'
+            },
+            validationData: {},
+            clientMetadata: {}
+        },
+        response: {
+            autoConfirmUser: false,
+            autoVerifyEmail: false,
+            autoVerifyPhone: false
+        }
+    };
+
+    const cognitoSocialLogin: PreSignUpExternalProviderTriggerEvent = {
+        version: '1',
+        region: 'eu-central-1',
+        userPoolId: 'eu-central-1_7HLvRT',
+        userName: 'Google_1147527301736',
+        callerContext: {
+            awsSdkVersion: 'aws-sdk-unknown-unknown',
+            clientId: '741if0rmdidv0bpsqc'
+        },
+        triggerSource: 'PreSignUp_ExternalProvider',
+        request: {
+            userAttributes: {
+                email_verified: 'false',
+                'cognito:email_alias': '',
+                'cognito:phone_number_alias': '',
+                given_name: 'test',
+                family_name: 'user',
+                email: 'test@gmail.com'
+            },
+            validationData: {}
+        },
+        response: {
+            autoConfirmUser: false,
+            autoVerifyEmail: false,
+            autoVerifyPhone: false
+        }
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.clearAllTimers();
+        mockListUsers = jest.fn(() => {
+            return {
+                promise: jest.fn(() =>
+                    Promise.resolve({
+                        Users: [
+                            {
+                                Username: 'test@gmail.com'
+                            }
+                        ]
+                    })
+                )
+            };
+        });
+
+        mockCreateUser = jest.fn(() => {
+            return {
+                promise: jest.fn(() => Promise.resolve({ User: { Username: 'a33faa43-4430-46b9-9604-54f42bd12d51' } }))
+            };
+        });
+    });
+
+    test('Not a external provider login', async () => {
+        const result = await handler(cognitoNativeEvent);
+        expect(result).toEqual(cognitoNativeEvent);
+    });
+
+    test('External provider login with existing cognito native user', async () => {
+        const result = await handler(cognitoSocialLogin);
+        expect(result).toEqual(cognitoSocialLogin);
+
+        expect(mockLinkUser).toHaveBeenCalledWith({
+            DestinationUser: {
+                ProviderAttributeValue: 'test@gmail.com',
+                ProviderName: 'Cognito'
+            },
+            SourceUser: {
+                ProviderAttributeName: 'Cognito_Subject',
+                ProviderAttributeValue: '1147527301736',
+                ProviderName: 'Google'
+            },
+            UserPoolId: 'eu-central-1_7HLvRT'
+        });
+        expect(mockLinkUser).toBeCalledTimes(1);
+        expect(mockListUsers).toBeCalledTimes(1);
+    });
+
+    test('No username for existing cognito user', async () => {
+        mockListUsers = jest.fn(() => {
+            return {
+                promise: jest.fn(() =>
+                    Promise.resolve({
+                        Users: [{}]
+                    })
+                )
+            };
+        });
+
+        await expect(handler(cognitoSocialLogin)).rejects.toThrowError('Username not found');
+    });
+
+    test('No native cognito user when using social login', async () => {
+        mockListUsers = jest.fn(() => {
+            return {
+                promise: jest.fn(() => Promise.resolve({}))
+            };
+        });
+
+        const result = await handler(cognitoSocialLogin);
+
+        expect(result.response.autoVerifyEmail).toBeTruthy();
+        expect(result.response.autoConfirmUser).toBeTruthy();
+        expect(mockLinkUser).toHaveBeenCalledWith({
+            DestinationUser: {
+                ProviderAttributeValue: 'a33faa43-4430-46b9-9604-54f42bd12d51',
+                ProviderName: 'Cognito'
+            },
+            SourceUser: {
+                ProviderAttributeName: 'Cognito_Subject',
+                ProviderAttributeValue: '1147527301736',
+                ProviderName: 'Google'
+            },
+            UserPoolId: 'eu-central-1_7HLvRT'
+        });
+        expect(mockSetUserPassword).toHaveBeenCalledWith(
+            expect.objectContaining({
+                UserPoolId: 'eu-central-1_7HLvRT',
+                Username: 'test@gmail.com',
+                Permanent: true,
+                Password: expect.any(String)
+            })
+        );
+        expect(mockLinkUser).toBeCalledTimes(1);
+        expect(mockSetUserPassword).toBeCalledTimes(1);
+        expect(mockListUsers).toBeCalledTimes(1);
+    });
+
+    test('No username when creating user', async () => {
+        mockListUsers = jest.fn(() => {
+            return {
+                promise: jest.fn(() => Promise.resolve({}))
+            };
+        });
+
+        mockCreateUser = jest.fn(() => {
+            return {
+                promise: jest.fn(() => Promise.resolve({}))
+            };
+        });
+
+        await expect(handler(cognitoSocialLogin)).rejects.toThrowError('Username not found');
     });
 });
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import { PreSignUpTriggerEvent } from 'aws-lambda';
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { AdminCreateUserResponse, ListUsersResponse } from 'aws-sdk/clients/cognitoidentityserviceprovider';
+import { generate } from 'generate-password';
+
+const EXTERNAL_AUTHENTICATION_PROVIDER = 'PreSignUp_ExternalProvider';
+
+async function getUsersByEmail(userPoolId: string, email: string, client: CognitoIdentityServiceProvider): Promise<ListUsersResponse> {
+    return client.listUsers({ UserPoolId: userPoolId, Filter: `email = "${email}"` }).promise();
+}
+
+async function linkUserAccounts(
+    cognitoUsername: string,
+    userPoolId: string,
+    providerName: string,
+    providerUserId: string,
+    client: CognitoIdentityServiceProvider
+) {
+    const params = {
+        DestinationUser: {
+            ProviderAttributeValue: cognitoUsername,
+            ProviderName: 'Cognito'
+        },
+        SourceUser: {
+            ProviderAttributeName: 'Cognito_Subject',
+            ProviderAttributeValue: providerUserId,
+            ProviderName: providerName
+        },
+        UserPoolId: userPoolId
+    };
+    await client.adminLinkProviderForUser(params).promise();
+}
+
+async function createUser(userPoolId: string, email: string, client: CognitoIdentityServiceProvider): Promise<AdminCreateUserResponse> {
+    return await client
+        .adminCreateUser({
+            UserPoolId: userPoolId,
+            MessageAction: 'SUPPRESS',
+            Username: email,
+            UserAttributes: [
+                { Name: 'email', Value: email },
+                { Name: 'email_verified', Value: 'true' }
+            ]
+        })
+        .promise();
+}
+
+async function setUserPassword(userPoolId: string, email: string, client: CognitoIdentityServiceProvider) {
+    await client
+        .adminSetUserPassword({
+            UserPoolId: userPoolId,
+            Username: email,
+            Password: generate({ length: 32, numbers: true, symbols: true }),
+            Permanent: true
+        })
+        .promise();
+}
+
+export async function handler(event: PreSignUpTriggerEvent): Promise<PreSignUpTriggerEvent> {
+    const client = new CognitoIdentityServiceProvider({ region: event.region });
+    const email = event.request.userAttributes.email;
+    const userPoolId = event.userPoolId;
+    if (event.triggerSource == EXTERNAL_AUTHENTICATION_PROVIDER) {
+        const usersFilteredByEmail = await getUsersByEmail(userPoolId, email, client);
+        const [providerNameValue, providerUserId] = event.userName.split('_');
+        const providerName = providerNameValue.charAt(0).toUpperCase() + providerNameValue.slice(1);
+
+        if (usersFilteredByEmail.Users && usersFilteredByEmail.Users.length > 0) {
+            const cognitoUsername = usersFilteredByEmail.Users[0].Username;
+            if (cognitoUsername === undefined) {
+                throw Error('Username not found');
+            }
+            await linkUserAccounts(cognitoUsername, userPoolId, providerName, providerUserId, client);
+        } else {
+            const newCognitoUser = await createUser(userPoolId, email, client);
+            await setUserPassword(userPoolId, email, client);
+
+            const cognitoNativeUsername = newCognitoUser.User?.Username;
+            if (cognitoNativeUsername === undefined) {
+                throw Error('Username not found');
+            }
+            await linkUserAccounts(cognitoNativeUsername, userPoolId, providerName, providerUserId, client);
+            event.response.autoVerifyEmail = true;
+            event.response.autoConfirmUser = true;
+        }
+    }
+    return event;
+}
